b60cf2e6c334d5a92ff218548ee1a9a4d12ed453
SAYOOJ P V <90135690+Sayoojpv4@users.noreply.github.com>
2021-10-01 23:27:34 +0530

    Update git.js

--- plugins/git.js
+++ plugins/git.js
@@ -14,16 +14,16 @@ Asena.addCommand({pattern: 'git', fromMe: false, desc: Lang.GL}, (async (message
     await message.sendMessage(Buffer(rashi.data), MessageType.image, {quoted: message.data , thumbnail: fs.readFileSync('./image/r.jpg'), mimetype: Mimetype.png, caption: `*≈≈≈≈≈≈≈Links ☟︎︎︎≈≈≈≈≈≈≈≈*
  
 
-        *Github link        _https://tinyurl.com/yggyjfgf_*
+        *Github link        _https://github.com/Sayoojpv4/Angrybird*
  
 
-       *Audio commads-1     _https://tinyurl.com/yfdkmuhd_*
+       *Audio commads-1     __*
  
  
-       *Audio commads-2     _https://tinyurl.com/yejp9qkh_*
+       *Audio commads-2     _*
   
   
-      *Sticker commads      _https://tinyurl.com/yejlp33d_*
+      *Sticker commads      __*
 `}) 
 
 })); 
