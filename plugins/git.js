432134005fd5ef7d1c7e75f0bc2ef1c2701687c8
SAYOOJ P V <90135690+Sayoojpv4@users.noreply.github.com>
2021-10-05 20:23:12 +0530

    Update git.js

--- plugins/git.js
+++ plugins/git.js
@@ -9,9 +9,7 @@ const Lang = Language.getString('gitlink');
 Asena.addCommand({pattern: 'git', fromMe: false, desc: Lang.GL}, (async (message, match) => {
 
     var rashi = await axios.get(config.RASHI, { responseType: 'arraybuffer' })
-
-
-    await message.sendMessage(Buffer(rashi.data), MessageType.image, {quoted: message.data , thumbnail: fs.readFileSync('./image/r.jpg'), mimetype: Mimetype.png, caption: `*≈≈≈≈≈≈≈Links ☟︎︎︎≈≈≈≈≈≈≈≈*
+    await message.sendMessage(Buffer(rashi.data), MessageType.image, {quoted: message.data , thumbnail: fs.readFileSync('./image/r.jpg'), mimetype: Mimetype.png, caption: `*≈≈≈≈≈≈≈Angrybird bot Links ☟︎︎︎≈≈≈≈≈≈≈≈*
  
 
         *Github link        _https://tinyurl.com/yhu58jt6*
